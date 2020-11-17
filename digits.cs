9c853126ace41fd68c93997043ced24e4c9bf081
Jose <cubiki_9@hotmail.com>
2020-11-17 21:04:41 +0100

    digits 2.0

--- digits.cs
+++ digits.cs
@@ -1,35 +1,36 @@
+//Author: JOSE CUBÍ ALBERT
+
 using System;
 
 class digits
 {
- static void Main()
- {
-	 //counters of number of digits a is for one digit, b is for 2 digits, c is for 3 digits and d is for more than 3 digits
-     int a = 0, b = 0, c = 0, d = 0;
-      Console.Write("Insert a number: ");
-      // read the number as string to check the word "end"
-      string e = Console.ReadLine();
-     while(e!="end"){
-int input = Convert.ToInt32(e);
-       if(input != 0)
-{
-       if(input/10 == 0)
-           a ++;  // it only has one digit
-       else 
-       if (input/100 == 0)
-		b ++;  // the number has 2 digits
-        else
-        if (input/1000 == 0)
-        c ++;  // the number has 3 digits
-        else
-           d++;  // the number has more than 3 digits
+    static void Main()
+    {
+        int unit = 0, ten = 0, hundred = 0, thousand = 0;
+        
+        Console.WriteLine("Insert a number (\"end\" for finish): ");
+        string dateEntered = Console.ReadLine();
+        
+        while(dateEntered != "end")
+        {
+            int input = Convert.ToInt32(dateEntered);
+            
+            if(input != 0)
+            {
+                if(input/10 == 0)
+                    unit++;  
+                else if (input/100 == 0)
+                    ten++; 
+                else if (input/1000 == 0)
+                    hundred++;  
+                else
+                    thousand++;  
+            }
+            dateEntered = Console.ReadLine();
         }
-      e = Console.ReadLine();
-}
 
-Console.WriteLine("one: {0} two: {1} three: {2} more: {3}", a, b, c, d); 
-
-
- }
+        Console.WriteLine("one: {0} two: {1} three: {2} more: {3}", unit, 
+        ten, hundred, thousand); 
+    }
 }
 
